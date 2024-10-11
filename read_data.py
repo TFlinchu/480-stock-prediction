4974581d4ff6c680b299632e6af68e1c7a02c7c1
Tyler Flinchum <tflinchu@emich.edu>
2024-10-10 22:26:32 -0400

    Finished implementing the read_data.py file
    This commit finished the implementation of the read_data.py file.
    The file is now able to read the data from the CSV file and return it as a list of dictionaries.
    There is some example code included in main.py, as we are not using it yet. Replace it when needed.
    
    * Renamed app.py to read_data.py
    * Added the main.py file

---
+++ read_data.py
@@ -0,0 +1,49 @@
+import glob
+import os
+import pandas as pd
+import torch
+
+def process_csv_files(stock):
+    # Path where the CSV files are stored, and set permissions
+    folder_path = './archive'
+    os.chmod(folder_path, 0o666)
+
+    # Get the CSV files matching the stock chosen
+    stock = stock + '.csv'
+    csv_file_path = os.path.join(folder_path, stock)
+
+    # Check if the .csv file exists
+    if not os.path.isfile(csv_file_path):
+        print(f"Error: The file {csv_file_path} does not exist.")
+    else:
+        csv_files = glob.glob(csv_file_path)
+
+    # Initialize a dictionary to store column data
+    column_data = {}
+
+    # Read each CSV file, drop stock splits & dividends, and store the data in the dictionary
+    for file in csv_files:
+        df = pd.read_csv(file)
+        df = df.drop(columns=['Stock Splits', 'Dividends'])
+        
+        # Convert 'Date' column to numeric format
+        # This may not be necessary, as I can't imagine we would need to use the date in our model
+        if 'Date' in df.columns:
+            df['Date'] = pd.to_datetime(df['Date']).astype('int64') / 10**9  # Convert to seconds since epoch
+
+        # Store the data in the dictionary
+        for column in df.columns:
+            if column not in column_data:
+                column_data[column] = []
+            column_data[column].extend(df[column].tolist())
+
+    # Convert lists in the dictionary to PyTorch tensors
+    tensor_data = {}
+    for key, value in column_data.items():
+        try:
+            tensor_data[key] = torch.tensor(value, dtype=torch.float32)
+        except ValueError:
+            # Should only happen if date column is not converted to numeric format
+            print(f"Skipping column {key} due to non-numeric data")
+
+    return tensor_data
